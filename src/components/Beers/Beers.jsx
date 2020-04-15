d274f22b24eb289031f60343460eaae7bd03257e
Diego Martinez <diegomtz1998@hotmail.com>
2020-04-15 12:33:04 -0500

    Add filtering by origin, brewery and style

--- src/components/Beers/Beers.jsx
+++ src/components/Beers/Beers.jsx
@@ -7,47 +7,31 @@ import { Beer } from './Beer';
 import "./style.scss";
 import { Filters } from './Filters';
 
-const { Title } = Typography;
-const { Header, Content, Footer } = Layout;
+const { Content, Footer } = Layout;
 
 const Beers = () => {
 
   const [beers, setBeers] = useState([]);
-  const [visible, setVisible] = useState(false);
+  const [filter, setFilter] = useState({});
 
   useEffect(() => {
-    axios.get('/beers')
-      .then((response) => {
-        setBeers(response.data);
-      }).catch((error) => {
-        message.error(error.statusText)
-      });
-  }, []);
+  axios.get('/beers', {
+    params: {
+      origin: filter.origin,
+      brewery: filter.brewery,
+      style: filter.style
+    }
+  })
+  .then((response) => {
+    setBeers(response.data);
+  }).catch((error) => {
+    message.error(error.statusText)
+  });
+  }, [filter]);
 
   return (
     <Layout>
-      <Header className="beers-header">
-          <Row>
-            <Col span={20}>
-              <Title className="beers-title" level={1}>Cervezas</Title>
-            </Col>
-            <Col span={4}>
-                <Button type="primary" onClick={() => setVisible(true)}>Filter <FilterOutlined /></Button>
-            </Col>
-          </Row>
-          <Drawer
-            title="Filters"
-            placement="top"
-            closable={false}
-            onClose={() => setVisible(false)}
-            visible={visible}
-            height={150}
-            getContainer={false}
-            style={{ position: 'absolute' }}
-          >
-            <Filters />
-          </Drawer>
-        </Header>
+      <Filters onSearch={setFilter}/>
       <Content className="beers-content">
         <Row>
           <Col>
