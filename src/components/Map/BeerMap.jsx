12de7791d8fddebd53c1d4092add9096aad0ce6a
Diego Martinez <diegomtz1998@hotmail.com>
2020-04-26 19:05:24 -0500

    Show markers and info

--- src/components/Map/BeerMap.jsx
+++ src/components/Map/BeerMap.jsx
@@ -1,15 +1,38 @@
 import { Layout } from 'antd';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import MapContainer from './MapContainer';
+import { message } from 'antd';
+import axios from 'axios';
 
 const { Content } = Layout;
 
 const BeerMap = () => {
 
+  const [places, setPlaces] = useState([{
+    name: "Beer For Us",
+    category: "Deposito",
+    lat: "25.643647",
+    lng: "-100.276135"
+  }, {
+    name: "Feroz",
+    category: "Bar",
+    lat: "25.689537",
+    lng: "-100.332932"
+  }]);
+  
+  // useEffect(() => {
+  //   axios.get('/places', {})
+  //   .then((response) => {
+  //     setPlaces(response.data)
+  //   }).catch((error) => {
+  //     message.error(error.statusText);
+  //   });
+  // }, []);
+
   return (
     <Layout>
       <Content>
-        <MapContainer />
+        <MapContainer places={places}/>
       </Content>
     </Layout>
   );
--- src/components/Map/MapContainer.jsx
+++ src/components/Map/MapContainer.jsx
@@ -1,23 +1,72 @@
-import React, { Component } from 'react';
-import { Map, GoogleApiWrapper } from 'google-maps-react';
+import React, { Component, } from 'react';
+import { Map, GoogleApiWrapper, InfoWindow, Marker } from 'google-maps-react';
 
-const mapStyles = {
+
+const mapStyle = {
   width: '80%',
   height: '80%'
 };
 
 export class MapContainer extends Component {
+  state = {
+    showingInfoWindow: false,  //Hides or the shows the infoWindow
+    activeMarker: {},          //Shows the active marker upon click
+    selectedPlace: {},         //Shows the infoWindow to the selected place upon a marker      
+  };
+  
+  onMarkerClick = (props, marker, e) => 
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+  
+  onClose = props => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      });
+    }
+  };
+
   render() {
     return (
       <Map
         google={this.props.google}
         zoom={12}
-        style={mapStyles}
+        style={mapStyle}
         initialCenter={{
          lat: 25.686613,
          lng: -100.316116
         }}
-      />
+      >
+        {console.log("PLACES:")}
+        {console.log(this.props.places)}
+        {this.props.places.map((place, index) => {
+          {console.log(place.name)}
+          return (
+            <Marker key={index} id={index} position={{
+              lat: place.lat,
+              lng: place.lng
+            }}
+            onClick={this.onMarkerClick}
+            name={place.name}
+            category={place.category} 
+            />
+          )
+        })}
+        <InfoWindow
+        marker={this.state.activeMarker}
+        visible={this.state.showingInfoWindow}
+        onClose={this.onClose}
+        >
+          <div>
+            <h4>{this.state.selectedPlace.name}</h4>
+            <h5>{this.state.selectedPlace.category}</h5>
+          </div>
+        </InfoWindow>
+      </Map>
     );
   }
 }
