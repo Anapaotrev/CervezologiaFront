5d418feb8baa9f16c9775787a5e9ca2bc9d2d3b4
Diego Martinez <diegomtz1998@hotmail.com>
2020-04-26 22:08:32 -0500

    Change to use menu in nav bar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
-import { Card, Typography, List, Descriptions, Button, Row, Col } from 'antd';
-import { Link } from 'react-router-dom';
-import React, { useContext } from 'react';
+import { Card, Typography, List, Descriptions, Button, Row, Col, Menu } from 'antd';
+import { Link, useLocation } from 'react-router-dom';
+import React, { useContext, useState } from 'react';
 import { UserContext } from '../../utils';
 import './style.scss';
 
@@ -8,29 +8,37 @@ const { Text, Title } = Typography;
 
 const Navbar = (props) => {
   const { isAuth, setUnauthStatus } = useContext(UserContext);
+  const location = useLocation();
+  const [currentKey, setCurrentKey] = useState(location.pathname);
+
   return (
-    <Row justify="end" align="middle">
-      <Col span={21}>
+    <Row align="middle">
+      <Col span={18}>
+        <img src={ require('../../assets/logo-color.png') } className="logo"/>
         <Text style={{ paddingTop: '5px', fontWeight: '800', fontSize: '20px', color: '#000000' }}>
           Cervezologia
         </Text>
       </Col>
-      <Col span={1}>
-        <Link to="/" className="link">HOME</Link>
-      </Col>
-      <Col span={1}>
-        | 
-        <Link to="/map" className="link"> MAP</Link>
-      </Col>
-      <Col span={1} justify="end">
+      <Menu 
+        onClick={(e) => setCurrentKey(e.key)} 
+        mode="horizontal" 
+        selectedKeys={[currentKey]} 
+        style={{ backgroundColor: 'rgb(240,242,245)'}}
+      >
+        <Menu.Item key='/'> 
+          <Link to="/" className="link">HOME</Link>
+        </Menu.Item>
+        <Menu.Item key='/map'>
+          <Link to="/map" className="link">MAP</Link>
+        </Menu.Item>
+        <Menu.Item key='/login'>
         {isAuth() ? (
           <Link onClick={() => setUnauthStatus()}>LOGOUT</Link>
         ) : (
-          <Link to="/login" className="login-button">
-            LOGIN
-          </Link>
+          <Link to="/login" className="login-button">LOGIN</Link>
         )}
-      </Col>
+        </Menu.Item>
+      </Menu>
     </Row>
   );
 };
